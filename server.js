bd5d1d4db4c19f3dcf63ea5d37723b225c4e7e53
Kyle LeBlanc <53625838+tiberiusnero560@users.noreply.github.com>
2021-04-27 00:09:43 -0400

    Completed Todo App
    This will be my fallback as I customize and butcher the code to make it my own and continue to learn. I will also take this as an opportunity to get more familiar with Git and Github with branches, and merging, etc. I will branch off new ideas and play with different concepts with this as a framework.

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const todoRoutes = express.Router();
+const PORT = 4000;
+
+let Todo = require('./todo.model');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// This is what I would be changing to my own instead of /todos to build my own application
+// This is the mongoDB constructor. I passed in useUnifiedTopoology as recommended by the warning messages
+mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+
+todoRoutes.route('/').get(function(req, res) {
+    Todo.find(function(err, todos) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(todos);
+        }
+    });
+});
+
+// The next endpoint which needs to be implemented is /:id. This path extension is used to retrieve a todo item by providing an ID.
+todoRoutes.route('/:id').get(function(req, res) {
+    let id = req.params.id;
+    Todo.findById(id, function(err, todo) {
+        res.json(todo);
+    });
+});
+
+// Finally a HTTP POST route /update/:id is added:
+todoRoutes.route('/update/:id').post(function(req, res) {
+    Todo.findById(req.params.id, function(err, todo) {
+        if (!todo)
+            res.status(404).send("data is not found");
+        else
+            todo.todo_description = req.body.todo_description;
+            todo.todo_responsible = req.body.todo_responsible;
+            todo.todo_priority = req.body.todo_priority;
+            todo.todo_completed = req.body.todo_completed;
+
+            todo.save().then(todo => {
+                res.json('Todo updated!');
+            })
+            .catch(err => {
+                res.status(400).send("Update not possible");
+            });
+    });
+});
+
+// Next, let’s add the route which is needed to be able to add new todo items by sending a HTTP post request (/add):
+todoRoutes.route('/add').post(function(req, res) {
+    let todo = new Todo(req.body);
+    todo.save()
+        .then(todo => {
+            res.status(200).json({'todo': 'todo added successfully'});
+        })
+        .catch(err => {
+            res.status(400).send('adding new todo failed');
+        });
+});
+
+app.use('/todos', todoRoutes);
+
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
